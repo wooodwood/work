2f1edead333a3d174aec1da106dfb10857240db0
zhouk <receivemsdn@126.com>
2019-07-10 12:05:48 +0800

    整理工程文件

---
+++ scrapy/lamp/env.py
@@ -0,0 +1,6 @@
+# coding=utf-8
+import logging as log
+
+if __name__ == '__main__':
+    log.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=log.INFO)
+    log.info("lamp scrapy ...")
